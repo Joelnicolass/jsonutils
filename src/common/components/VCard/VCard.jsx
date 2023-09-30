9cd1a83a99614688d25046ddf4b6c44e2b4dbb19
joelsartori <joelnicolass@gmail.com>
2023-09-29 21:55:25 -0300

    fix cards

--- src/common/components/VCard/VCard.jsx
+++ src/common/components/VCard/VCard.jsx
@@ -1,36 +1,17 @@
 import React, { useEffect } from "react";
 
 const VCard = () => {
-  const vcard = `BEGIN:VCARD
-    VERSION:3.0
-    PRODID:-//Apple Inc.//iPhone OS 16.6.1//EN
-    N:Macheo Lorca;Francisco E.;;;
-    FN:Francisco E. Macheo Lorca
-    ORG:ASUN;
-    EMAIL;type=INTERNET;type=WORK;type=pref:fmacheo@asun.cl
-    EMAIL;type=INTERNET;type=WORK:fmacheo@asunarg.com
-    TEL;type=CELL;type=VOICE;type=pref:+54 9 3764 57 6176
-    X-SOCIALPROFILE;type=facebook:http://www.facebook.com/Facebook@AsunChile
-    X-SOCIALPROFILE;type=Instagram;x-user=aserraderos_unidos_asun:x-apple:aserraderos_unidos_asun
-    item1.URL;type=pref:www.asun.cl
-    item1.X-ABLabel:$!<HomePage>!$
-    URL;type=Created with:https://vcard.nfc.cool
-    END:VCARD`;
-
+  // download file from drive
   useEffect(() => {
-    console.log(vcard);
-    const file = new Blob([vcard], { type: "text/plain" });
-    const a = document.createElement("a");
-    const url = URL.createObjectURL(file);
-    a.href = url;
-    a.download = "vcard.vcf";
-    a.click();
-    URL.revokeObjectURL(url);
+    const url =
+      "https://drive.google.com/file/d/16mZggV9bnNzEaT-zfQ7dr7dx5dGtUtSs/view?usp=share_link";
+
+    window.open(url, "_blank");
   }, []);
 
   return (
     <div>
-      <h1>descargando</h1>
+      <h1>VCard</h1>
     </div>
   );
 };
